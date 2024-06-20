4855110e00856b38003fb0d826ad43729c25f71e
Victor Fernandes <victorfariafernandes@gmail.com>
2024-06-20 13:19:29 -0300

    update

--- gauss-ckks_.py
+++ gauss-ckks_.py
@@ -5,7 +5,7 @@ secret_key_dist = SecretKeyDist.UNIFORM_TERNARY
 parameters.SetSecretKeyDist(secret_key_dist)
 
 parameters.SetSecurityLevel(SecurityLevel.HEStd_NotSet)
-parameters.SetRingDim(1<<10)
+parameters.SetRingDim(1<<8)
 
 # if get_native_int()==128:
 #     rescale_tech = ScalingTechnique.FIXEDAUTO
@@ -22,11 +22,11 @@ parameters.SetFirstModSize(first_mod)
 
 level_budget = [4, 4]
 
-levels_available_after_bootstrap = 60
+levels_available_after_bootstrap = 120
 
 depth = levels_available_after_bootstrap + FHECKKSRNS.GetBootstrapDepth(level_budget, secret_key_dist)
 
-parameters.SetMultiplicativeDepth(50)
+parameters.SetMultiplicativeDepth(depth)
 
 cc = GenCryptoContext(parameters)
 cc.Enable(PKESchemeFeature.PKE)
@@ -38,8 +38,8 @@ cc.Enable(PKESchemeFeature.FHE)
 ring_dim = cc.GetRingDimension()
 # This is the mazimum number of slots that can be used full packing.
 
-matriz_exemplo = [[1.9, 1.6, 1.8, 1.1, 0.6], [1.2, 1.9, 1.6, 1.2, 0.7], [1.1, 1.2, 1.9, 1.1, 0.5], [1.2, 1.9, 1.3, 2.3, 1.2], [1.1, 1.2, 1.9, 1.9, 2.4]]
-b_exemplo = [1.3, 1.2, 1.7, 1.3, 1.9]
+matriz_exemplo = [[1.9 , 1.6, 1.8], [1.2, 1.9 , 1.6], [1.1, 1.2, 1.9 ]]
+b_exemplo = [1.3 , 5, 1.7]
 
 num_slots = int(ring_dim/2)
 print(f"CKKS is using ring dimension {ring_dim}")
@@ -51,7 +51,11 @@ cc.EvalMultKeyGen(keys.secretKey)
 cc.EvalBootstrapKeyGen(privateKey=keys.secretKey,  slots=num_slots)
 cc.EvalRotateKeyGen(keys.secretKey, list(range(-(num_slots + 1), num_slots+ 1)))
 
-
+# [1,0,0,0,0]
+# [0,1,0,0,0]
+# [0,0,1,0,0]
+# [3,0,0,1,0] [3,0,0,0,0] [3,0,0,0,0] [3,0,0,0,0] [3,0,0,0,0]
+# [3,3,3,3,1]
 def getCipherTextAtSlot(ciphertext,  i):
     mask = [0] * num_slots
     mask[i] = 1
@@ -64,8 +68,6 @@ def getCipherTextAtSlot(ciphertext,  i):
     r_merge = [result] * num_slots
 
     result = cc.EvalMerge(r_merge)
-    # print("input", i, decrypt(ciphertext,keys.secretKey))
-    # print("result", i, decrypt(result,keys.secretKey))
 
     return result
 
@@ -99,7 +101,7 @@ for i in range(len(b_exemplo)):
 def divisao_linha(h, divisor):
     d = cc.EvalMultAndRelinearize(divisor, 1, 256, 129)
     #d = cc.EvalBootstrap(d)
-    h = cc.EvalMult(h,d)
+    h = cc.EvalMultAndRelinearize(h,d)
     #h = cc.EvalBootstrap(h)
 
     return h
@@ -113,47 +115,60 @@ def subtracao(h1, h2, coef):
 
     return h1
 
+def updateB(b, line, i, j):
+    cb = getCipherTextAtSlot(b, i)# [b[i], b[i], ..., b[i]]
+    cline = getCipherTextAtSlot(line, i) # [line[i], line[i], ..., line[i]]
+
+    r = cc.EvalMultAndRelinearize(cb, cline)# [b[i] * line[i], b[i] * line[i], ..., b[i] * line[i]]
+
+    mask = [0] * num_slots
+    mask[j] = 1
+
+    mask_ciphertext = cc.Encrypt(keys.publicKey, cc.MakeCKKSPackedPlaintext(mask)) # [0, 0, ..., 1, 0, 0] na posição j
+    result_at_j = cc.EvalMultAndRelinearize(r, mask_ciphertext) # [0, 0, ..., b[i] * line[i], 0, 0] na posição j
+
+    return cc.EvalSub(b, result_at_j) # b - [b[i], b[i], ..., b[i] * line[i], b[i], b[i]] na posição j
+    
+def updateBDivision(b, dividend, i):
+    cb = getCipherTextAtSlot(b, i)# [b[i], b[i], ..., b[i]]
+
+    r = cc.EvalMultAndRelinearize(cb, dividend)# [b[i] * line[i], b[i] * line[i], ..., b[i] * line[i]]
+
+    mask1 = [0] * num_slots
+    mask1[i] = 1
+    mask1_ciphertext = cc.Encrypt(keys.publicKey, cc.MakeCKKSPackedPlaintext(mask1)) # [0, 0, ..., 1, 0, 0] na posição j
+    
+    mask2 = [1] * num_slots
+    mask2[i] = 0
+    mask2_ciphertext = cc.Encrypt(keys.publicKey, cc.MakeCKKSPackedPlaintext(mask2))
+    
+    b_without_i = cc.EvalMultAndRelinearize(b, mask2_ciphertext) # [0, 0, ..., b[i] * line[i], 0, 0] na posição j
+    r_with_i = cc.EvalMultAndRelinearize(r, mask1_ciphertext) # [0, 0, ..., b[i] * line[i], 0, 0] na posição j
+
+    return cc.EvalAdd(b_without_i, r_with_i) # b - [b[i], b[i], ..., b[i] * line[i], b[i], b[i]] na posição j
+    
+
+
 def gauss(matrix, b, M):
     for i in range(M):
-        # #print("noise budget before", matrix[i].GetNoiseScaleDeg(), i)
-        #print("m[i]", i, decrypt(matrix[i],keys.secretKey))
-        #print("m[i][i]", i, decrypt(getCipherTextAtSlot(matrix[i], i),keys.secretKey))
-        print(i, decrypt(matrix[i], keys.secretKey))
         d = cc.EvalDivide(getCipherTextAtSlot(matrix[i], i), 1, 256, 129)
-        print("(d)",i,  decrypt(d, keys.secretKey))
-
-        #print("before (m[i])",i,  decrypt(matrix[i], keys.secretKey))
         matrix[i] = cc.EvalMultAndRelinearize(matrix[i],d)
         matrix[i] = cc.EvalBootstrap(matrix[i])
-        print(i, decrypt(matrix[i], keys.secretKey))
 
-        #print("noise budget after", matrix[i].GetNoiseScaleDeg(), i)
-        #print("iteration ", i)
+        b = updateBDivision(b, d, i)
+        b = cc.EvalBootstrap(b)
+
         for j in range(M):
             if (i != j):
-                # b[j] -= b[i] * matrix[j][i]
-                # p = cc.EvalMult(b, getCipherTextAtSlot(matrix[j], i))
-                # p = cc.EvalBootstrap(p)
-                # b[j] = cc.EvalSub(b[j], p)
-                # b[j] = cc.EvalBootstrap(b[j])
-                # b[j] = cc.EvalBootstrap(b[j])
-                #print("noise budget before", matrix[i].GetNoiseScaleDeg(), j)
-                #print("(m[j])", j, decrypt(matrix[j], keys.secretKey))
-                #print("(m[j][i])", j, decrypt(getCipherTextAtSlot(matrix[j], i), keys.secretKey))
-                print(i, j,  decrypt(matrix[j], keys.secretKey))
+                b = updateB (b, matrix[j], i, j)
                 g = cc.EvalMultAndRelinearize(matrix[i], getCipherTextAtSlot(matrix[j], i))
                 matrix[j] = cc.EvalSub(matrix[j], g)
                 matrix[j] = cc.EvalBootstrap(matrix[j])
-                print(i, j,  decrypt(matrix[j], keys.secretKey))
-
-                #print("noise budget after", matrix[i].GetNoiseScaleDeg(), j)
-                #matrix[j] = cc.ReEncrypt(matrix[j])
 
     return b
 
-gauss(encrypted_A, encrypted_B, len(encrypted_A))
+b_result = gauss(encrypted_A, encrypted_B, len(encrypted_A))
 
-#print(f"Number of levels remaining: {depth - encrypted_A[0][0].GetLevel()}")
 
 ############
 
@@ -162,14 +177,11 @@ decrypted_B = []
 
 for i in range(len(b_exemplo)):
     decrypted_A.append(decrypt(encrypted_A[i], keys.secretKey))
-    #print(i)
-    #decrypted_B.append(decrypt(encrypted_B[i], keys.secretKey))
-    # for j in range(len(b_exemplo)):
-    #decrypted_A[i].append(decrypt(encrypted_A[i][j], keys.secretKey))
 
+decrypted_B.append(decrypt(b_result, keys.secretKey))
 
 print(decrypted_A)
-#print(decrypted_B)
+print("B", decrypted_B)
 # result = cc.Decrypt(c_add,keys.secretKey)
 # result.SetLength(batch_size)
 # print("c1 + c2 = " + str(result))
--- gauss.py
+++ gauss.py
@@ -1,32 +1,35 @@
-def divisao_linha(h, divisor, m):
-    for i in range(m):
-        h[i] = h[i]/ divisor
-    return h
+def divisao_linha(h, divisor):
+    """Divide uma linha da matriz pelo divisor"""
+    return [elemento / divisor for elemento in h]
 
+def subtracao(h1, h2, coef):
+    """Subtrai uma linha da matriz multiplicada por um coeficiente de outra linha"""
+    return [elem1 - coef * elem2 for elem1, elem2 in zip(h1, h2)]
 
-def subtracao(h1, h2, coef, m):
-    for i in range(m):
-        h1[i] -= (h2[i] * coef)
+def gauss(A, b):
+    """Aplica o algoritmo de eliminação de Gauss na matriz A e no vetor b"""
+    n = len(A)
+    
+    # Aplicar eliminação de Gauss
+    for i in range(n):
+        # Dividir a linha i pelo elemento A[i][i]
+        divisor = A[i][i]
+        A[i] = divisao_linha(A[i], divisor)
+        b[i] /= divisor
 
-    return h1
-
-def gauss(matrix, b, M):
-
-    for i in range(M):
-        # print(i, matrix[i])
-        matrix[i] = divisao_linha(matrix[i], matrix[i][i], M) 
-        # print(i, matrix[i])
-        for j in range(M):
-            if (i != j):
-                b[j] -= b[i] * matrix[j][i]
-                # print(i, j, matrix[j])
-                matrix[j] = subtracao(matrix[j], matrix[i], matrix[j][i], M)
-                # print(i, j, matrix[j])
+        print(b)
+        
+        # Subtrair a linha i das outras linhas
+        for j in range(n):
+            if i != j:
+                coef = A[j][i]
+                A[j] = subtracao(A[j], A[i], coef)
+                b[j] -= coef * b[i]
+    
     return b
         
-matriz_exemplo = [[1.9, 1.6, 1.8], [1.2, 1.9, 1.6], [1.1, 1.2, 1.9]]
-b_exemplo = [1.3, 1.2, 1.7]
+matriz_exemplo = [[0 , 1.6, 1.8], [1, 1.9 , 1.6], [1, 1.2, 1.9 ]]
+b_exemplo = [0 , 5, 1.7]
 
 
-gauss(matriz_exemplo, b_exemplo, len(b_exemplo))
-print(matriz_exemplo)
+print(gauss(matriz_exemplo, b_exemplo))
